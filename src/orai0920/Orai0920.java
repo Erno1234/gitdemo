22324cd29e4894d4d713da92a7b419159021eea6
Erno1234 <palocierno12@gmail.com>
2023-09-20 12:50:00 +0200

    alap letisztazva

--- src/orai0920/Orai0920.java
+++ src/orai0920/Orai0920.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package orai0920;
 
-/**
- *
- * @author PálóciErnő(SZF_2022_
- */
+
 public class Orai0920 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
